67a42037f7a443beb1ee82417539bc844ed97ed8
quinncidences <qrc.inpa@gmail.com>
2019-05-24 16:03:09 -0700

    got post and user forms properly persisting to the db

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -10,20 +10,9 @@ class PostsController < ApplicationController
     render json: @post
   end
 
-  # def show
-  #   @post = Post.find(params[:id])
-  #   render json: @post
-  # end
-
-  # def update
-  #   @post = Post.find(params[:id])
-  #   @post.update(post_params)
-  #   render json: @post
-  # end
-  #
-  # def destroy
-  #   @post = Post.find(params[:id])
-  #   @post.destroy
-  # end
+  private
+  def post_params
+    params.require(:post).permit(:content, :user_id)
+  end
 
 end
