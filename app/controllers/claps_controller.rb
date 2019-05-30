181cc12bca38b8c2a428dec26f5d60c1493a23a0
quinncidences <qrc.inpa@gmail.com>
2019-05-29 17:13:27 -0700

    pushed changes for claps and formatting

--- app/controllers/claps_controller.rb
+++ app/controllers/claps_controller.rb
@@ -1,5 +1,5 @@
 class ClapsController < ApplicationController
-  skip_before_action :authorized, only: [:create, :index]
+  skip_before_action :authorized, only: [:create, :index, :destroy]
 
   def index
     @claps = Clap.all
@@ -12,8 +12,14 @@ class ClapsController < ApplicationController
   end
 
   def destroy
-    @clap = Clap.find(id)
-    @clap.destroy
+    @user_id = params[:clap][:user_id].to_i
+    @post_id = params[:clap][:post_id].to_i
+    puts "USERID", @user_id, @user_id.class
+    puts "POSTID", @post_id, @post_id.class
+    @clap = Clap.select do |c| c.user_id == @user_id && c.post_id == @post_id end
+    puts 'CLAP', @clap
+    @clap[0].destroy
+    # redirect_back(fallback_location: root_path)
   end
 
 
